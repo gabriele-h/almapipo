9f3c2f5d8ceeb92e86f03881c8d228cfe375b862
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-02-11 14:18:05 +0100

    Test for list of Alma IDs

--- tests/test_read_input.py
+++ tests/test_read_input.py
@@ -1,5 +1,10 @@
-"""Tests for alma_rest"""
+"""Tests for read_input"""
 
 import pytest
 
-import read_csv
+import read_input
+
+set_of_alma_ids = {'9981093873903332', '22447985240003332', '23447985190003332', '61363367390003332', '62363367380003332', '53375846240003332', '81445078000003332'}
+
+def test_does_list_contain_alma_ids_only():
+   read_input.does_list_contain_alma_ids_only(set_of_alma_ids)
