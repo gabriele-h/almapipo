0c4a292439896118e9fc8fff9c2e844ad19ae487
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-02-18 17:02:09 +0100

    Read whole csv instead of just first column

--- read_input.py
+++ read_input.py
@@ -11,7 +11,7 @@ manipulations.
 import re
 import sys
 from os import path, access, R_OK
-from csv import reader
+from csv import DictReader
 
 alma_id_pattern_str = r"^(22|23|53|61|62|81|99)\d{2,}3332$"
 pattern = re.compile(alma_id_pattern_str)
@@ -19,11 +19,10 @@ pattern = re.compile(alma_id_pattern_str)
 
 def main():
    print("Use as commandline-tool only to test a given list of IDs.")
-   print("Provide the csv-file you want to test as ARGV1.")
+   print("Provide the csv-file (semicolon-separated) or tsv-file as ARGV1.")
    print("---")
    csv_path = set_csv_path_from_argv1()
-   id_list = read_csv_contents_to_list(csv_path)
-
+   lists_from_csv = read_csv_contents_to_list(csv_path)
 
 def set_csv_path_from_argv1() -> str:
    try:
@@ -32,22 +31,26 @@ def set_csv_path_from_argv1() -> str:
       print('Please provide CSV-file as argument.')
    else:
       if not path.exists(argv1):
-         print(f'File {argv} does not exist.')
+         print(f'File {argv1} does not exist.')
       elif not access(argv1, R_OK):
          print(f'File {argv1} is not readable.')
-      elif argv1[-4:] != '.csv':
-         print(f'File {argv1} does not seem to be a csv-file?')
+      elif argv1[-4:] != '.csv' and argv1[-4:] != '.tsv':
+         print(f'File {argv1} does not seem to be a csv- or tsv-file?')
       else:
          return sys.argv[1]
 
 
 def read_csv_contents_to_list(csv_path) -> list:
-   id_list = []
+   list_of_dicts=[]
+   if csv_path[-4:] == '.csv':
+      delimit = ';'
+   elif csv_path[-4:] == '.tsv':
+      delimit = '\t'
    with open(csv_path, newline="") as csv_file:
-      csv_reader = reader(csv_file, delimiter=';', quotechar='"')
+      csv_reader = DictReader(csv_file, delimiter=delimit)
       for row in csv_reader:
-         id_list.append(row[0])
-   return id_list
+         list_of_dicts.append(row)
+   return list_of_dicts
 
 
 def is_this_an_alma_id(identifier) -> bool:
