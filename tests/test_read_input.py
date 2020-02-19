0b2f867ff01bdd25c22cb2a3c46cc0a97eb2c278
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-02-19 15:26:03 +0100

    Make tests match single id check

--- tests/test_read_input.py
+++ tests/test_read_input.py
@@ -5,27 +5,31 @@ import pytest
 import read_input
 
 
-class TestDoesListContainAlmaIdsOnly:
+class TestDoesStringEqualAlmaID:
    set_of_alma_ids = {
       '9981093873903332', '22447985240003332', '23447985190003332',
       '61363367390003332', '62363367380003332', '53375846240003332',
       '81445078000003332'
    }
-   set_of_alma_ids_containing_other = {
-      '9981093873903332', '22447985240003332', '23447985190003332',
-      '61363367390003332', '62363367380003332', '53375846240003332',
-      '81445078000003332', '813332'
-   }
-   set_empty = {}
+   not_an_alma_id = '813332'
+   string_empty = ""
+   not_a_string = 1234567
 
    def test_alma_ids_only(self):
-      bool = read_input.does_list_contain_alma_ids_only(self.set_of_alma_ids)
+      bool = all(read_input.is_this_an_alma_id(id) for id in self.set_of_alma_ids)
       assert bool == True
    
    def test_containing_other(self):
-      bool = read_input.does_list_contain_alma_ids_only(self.set_of_alma_ids_containing_other)
+      bool = read_input.is_this_an_alma_id(self.not_an_alma_id)
       assert bool == False
    
    def test_empty(self):
-      bool = read_input.does_list_contain_alma_ids_only(self.set_empty)
+      bool = read_input.is_this_an_alma_id(self.string_empty)
       assert bool == False
+
+   def test_number_not_string(self):
+      bool = read_input.is_this_an_alma_id(self.not_a_string)
+      assert bool == False
+
+   def test_set_not_string_either(self):
+      bool = read_input.is_this_an_alma_id(self.set_of_alma_ids)
