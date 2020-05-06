d6e6626ddceec9fefbbb6579eb8d45fe2f86b4dc
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-05-06 11:59:28 +0200

    Switch to SQLAlchemy ORM

--- db_read_write.py
+++ db_read_write.py
@@ -11,7 +11,8 @@ import db_setup
 from datetime import datetime
 from typing import OrderedDict
 
-from sqlalchemy import create_engine, engine, select
+from sqlalchemy import create_engine, engine
+from sqlalchemy.orm import sessionmaker
 
 import logfile_setup
 
@@ -31,25 +32,43 @@ def main():
     """
     logfile_setup.log_to_stdout(logger)
 
-    connection_params = db_setup.prepare_connection_params_from_env()
-    db_engine = create_engine(connection_params, echo=True)
-
+    db_engine = create_db_engine()
     db_engine.connect()
 
 
-def copy_lines_to_source_csv_table(csv_line: OrderedDict, db_engine: engine):
+def copy_lines_to_source_csv_table(csv_line: OrderedDict):
     """
     For an ordered Dictionary of values retrieved from a csv/tsv file
     create an entry in the database that identifies the job
     responsible for the entry (job_id).
     :param csv_line: Ordered dictionary of values from a line of the input file.
-    :param db_engine: database engine as created via db_setup.py
     :return: None
     """
-    table_source_csv = db_setup.TableDefiner.define_source_csv()
-    ins = table_source_csv.insert().values(job_id=job_id, csv_line=csv_line)
-    conn = db_engine.connect()
-    conn.execute(ins)
+    session = create_db_session()
+    line_for_table_source_csv = db_setup.SourceCsv(job_date=job_id, csv_line=csv_line)
+    session.add(line_for_table_source_csv)
+    session.commit()
+
+
+def create_db_session():
+    """
+    Create a DB session to manipulate the contents of the DB.
+    :return: Session for connection to the DB.
+    """
+    db_engine = create_db_engine()
+    Session = sessionmaker(bind=db_engine)
+    session = Session()
+    return session
+
+
+def create_db_engine():
+    """
+    Create the DB engine according to the information provided in env vars.
+    :return: DB engine.
+    """
+    connection_params = db_setup.prepare_connection_params_from_env()
+    db_engine = create_engine(connection_params, echo=True)
+    return db_engine
 
 
 if __name__ == "__main__":
