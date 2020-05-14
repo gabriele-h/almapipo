34e97115fe28111cb45e99220fde58d8efa884a7
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-05-14 15:09:34 +0200

    Make response accessible seperately
    In case other parts of the response need to be accessed.

--- xml_extract.py
+++ xml_extract.py
@@ -22,6 +22,7 @@ def extract_xml_record_element_from_fetched_records(alma_ids: str) -> Element:
     :param alma_ids: Comma separated string of Alma IDs to identify the record.
     :return: Element of the record's XML.
     """
+    logger.info(f'Converting string of XML for {alma_ids} to xml.etree Element.')
     xml_as_string = extract_xml_from_fetched_records(alma_ids)
     xml_as_element = fromstring(xml_as_string)
     return xml_as_element
@@ -34,14 +35,26 @@ def extract_xml_from_fetched_records(alma_ids: str) -> str:
     :param alma_ids: Comma separated string of Alma IDs to identify the record.
     :return: String of the record's XML.
     """
-    logger.info(f'Extracting most recent record with alma_ids {alma_ids} from table fetched_records.')
-    record_query = db_read_write.get_record_from_fetched_records(alma_ids)
-    record_as_bytes = record_query.alma_record
-    record_as_string = str(record_as_bytes, 'utf-8')
+    logger.info(f'Extracting XML from response for {alma_ids} as string.')
+    response_as_string = extract_response_from_fetched_records(alma_ids)
     try:
-        record_as_json = json.loads(record_as_string)
+        response_as_json = json.loads(response_as_string)
     except TypeError:
         logger.error('Data type not valid to load as JSON object.')
     else:
-        xml_string = record_as_json['anies'][0]
+        xml_string = response_as_json['anies'][0]
         return xml_string
+
+
+def extract_response_from_fetched_records(alma_ids: str) -> str:
+    """
+    From the table fetched_records extract the whole response for the record
+    and convert it from byte-array to string.
+    :param alma_ids: Comma separated string of Alma IDs to identify the record.
+    :return: String of the record's XML.
+    """
+    logger.info(f'Extracting most recent response for alma_ids {alma_ids} from table fetched_records.')
+    response_query = db_read_write.get_record_from_fetched_records(alma_ids)
+    response_as_bytes = response_query.alma_record
+    response_as_string = str(response_as_bytes, 'utf-8')
+    return response_as_string
