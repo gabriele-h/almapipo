80002156b4b7f93b316dab55a1e0aed8ac6c6101
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-05-14 15:00:21 +0200

    Correct extract_xml_from_fetched_records

--- xml_extract.py
+++ xml_extract.py
@@ -22,6 +22,13 @@ def extract_xml_from_fetched_records(alma_ids: str):
     :param alma_ids: Comma separated string of Alma IDs to identify the record.
     :return: Element of the record's XML.
     """
+def extract_xml_from_fetched_records(alma_ids: str) -> str:
+    """
+    From the table fetched_records extract the XML of the record and
+    return it as a string.
+    :param alma_ids: Comma separated string of Alma IDs to identify the record.
+    :return: String of the record's XML.
+    """
     logger.info(f'Extracting most recent record with alma_ids {alma_ids} from table fetched_records.')
     record_query = db_read_write.get_record_from_fetched_records(alma_ids)
     record_as_bytes = record_query.alma_record
@@ -31,7 +38,5 @@ def extract_xml_from_fetched_records(alma_ids: str):
     except TypeError:
         logger.error('Data type not valid to load as JSON object.')
     else:
-        print(type(record_as_json))
         xml_string = record_as_json['anies'][0]
-        xml_element = fromstring(xml_string)
-        return xml_element
+        return xml_string
