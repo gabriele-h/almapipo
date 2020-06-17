f6673b1839ca5dbffbd2fc6079c8de509e6e4f18
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-06-17 13:18:20 +0200

    Remove some functions that are now redundant
    With the change of the table-column format of alma_record in
    fetched_records, it is no longer necessary to manipulate the
    query's output. Querying for fetched_records will always
    return an ElementTree now.

--- xml_extract.py
+++ xml_extract.py
@@ -14,38 +14,11 @@ import logfile_setup
 logger = getLogger(__name__)
 
 
-def extract_record_from_fetched_records(alma_ids: str) -> ElementTree.Element:
-    """
-    From table fetched_records extract the XML of the record and
-    return it as an xml.etree Element.
-    :param alma_ids: Comma separated string of Alma IDs to identify the record.
-    :return: Element of the record's XML.
-    """
-    logger.info(f'Searching for record within XML response for {alma_ids}.')
-    response_xml = extract_response_from_fetched_records(alma_ids)
-    record_as_etree = response_xml.findall('record')
-    return record_as_etree
-
-
-def extract_response_as_bytes_from_fetched_records(alma_ids: str) -> bytes:
-    """
-    From the table fetched_records extract the whole response for the record
-    and convert the ElementTree to bytes.
-    :param alma_ids: Comma separated string of Alma IDs to identify the record.
-    :return: bytes of the record's GET response content
-    """
-    logger.info(f'Converting XML response for {alma_ids} to bytes.')
-    response_xml = extract_response_from_fetched_records(alma_ids)
-    record_as_bytes = ElementTree.tostring(response_xml)
-    return record_as_bytes
-
-
 def extract_response_from_fetched_records(alma_ids: str) -> ElementTree:
     """
-    From the table fetched_records extract the whole response for the record
-    and convert it from byte-array to string.
+    From the table fetched_records extract the whole response for the record.
     :param alma_ids: Comma separated string of Alma IDs to identify the record.
-    :return: ElementTree of the GET response in XML format for the record.
+    :return: ElementTree of the record.
     """
     logger.info(f'Extracting most recent response for alma_ids {alma_ids} from table fetched_records.')
     response_query = db_read_write.get_record_from_fetched_records(alma_ids)
