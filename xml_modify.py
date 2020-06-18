a92c5ae5f3266d59b36cf1e7d460ea8bfa748689
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-06-18 10:37:45 +0200

    Add function to add element to etree

--- xml_modify.py
+++ xml_modify.py
@@ -5,12 +5,39 @@ Does some generic XML modifications like "remove element" or "replace element co
 from copy import deepcopy
 from logging import getLogger
 from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
 
 # noinspection PyUnresolvedReferences
 import logfile_setup
 
 # Logfile
 logger = getLogger(__name__)
+
+
+def add_element_to_xml(
+        xml: ElementTree,
+        element_tag: str,
+        element_attributes: dict = None,
+        element_text: str = None) -> ElementTree:
+    """
+    For an xml given as an ElementTree, add the given Element.
+    The Element will be added as a direct descendant of the root element.
+    The original xml fed to the function will be untouched by this operation.
+    :param xml: ElementTree of the xml to be manipulated.
+    :param element_tag: Type of tag of the element to be added.
+    :param element_attributes: Attributes of the element to be added.
+    :param element_text: Text of the elemet to be added.
+    :return: ElementTree of the manipulated xml.
+    """
+    manipulated_xml = deepcopy(xml)
+    if element_attributes:
+        element = Element(element_tag, element_attributes)
+    else:
+        element = Element(element_tag)
+    if element_text:
+        element.text = element_text
+    manipulated_xml.append(element)
+    return manipulated_xml
 
 
 def remove_element_from_xml_by_tag(xml: ElementTree, element_tag: str) -> ElementTree:
