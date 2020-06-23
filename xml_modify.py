d3f0287b33c414fdf04547980b1006cc6afdb5ca
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-06-23 14:30:45 +0200

    Change order of parameters

--- xml_modify.py
+++ xml_modify.py
@@ -17,16 +17,16 @@ logger = getLogger(__name__)
 def add_element_to_root(
         xml: ElementTree,
         element_tag: str,
-        element_attributes: dict = None,
-        element_text: str = None) -> ElementTree:
+        element_text: str = None,
+        element_attributes: dict = None) -> ElementTree:
     """
     For an xml given as an ElementTree, add the given Element.
     The Element will be added as a direct descendant of the root element.
     The original xml fed to the function will be left untouched by this operation.
     :param xml: ElementTree of the xml to be manipulated
     :param element_tag: Type of tag of the element to be added
-    :param element_attributes: Attributes of the element to be added
     :param element_text: Text of the element to be added
+    :param element_attributes: Attributes of the element to be added
     :return: ElementTree of the manipulated xml
     """
     manipulated_xml = deepcopy(xml)
@@ -39,8 +39,8 @@ def add_element_to_child(
         xml: ElementTree,
         child: str,
         element_tag: str,
-        element_attributes: dict = None,
-        element_text: str = None) -> ElementTree:
+        element_text: str = None,
+        element_attributes: dict = None) -> ElementTree:
     """
     For an xml given as an ElementTree, add the given Element to a specific child tag.
     The Element will be added to all children matching the description given for child.
@@ -48,8 +48,8 @@ def add_element_to_child(
     :param xml: ElementTree of the xml to be manipulated
     :param child: Path to the child as per Element.findall() - e. g. tag type
     :param element_tag: Type of tag of the element to be added
-    :param element_attributes: Attributes of the element to be added
     :param element_text: Text of the element to be added
+    :param element_attributes: Attributes of the element to be added
     :return: ElementTree of the manipulated xml
     """
     manipulated_xml = deepcopy(xml)
@@ -62,10 +62,10 @@ def add_element_to_child(
 def replace_element_(
         xml: ElementTree,
         element_tag: str,
-        old_element_attributes: dict = None,
-        new_element_attributes: dict = None,
         old_element_text: str = None,
-        new_element_text: str = None) -> ElementTree:
+        new_element_text: str = None,
+        old_element_attributes: dict = None,
+        new_element_attributes: dict = None) -> ElementTree:
     """
     In an xml given as an ElementTree, replace one element by another.
     Please note that you will have to provide all attributes of the element
@@ -74,10 +74,10 @@ def replace_element_(
     The original xml fed to the function will be left untouched by this operation.
     :param xml: ElementTree of the xml to be manipulated
     :param element_tag: Type of tag of the element to be replaced
-    :param old_element_attributes: Attributes of the element to be replaced
-    :param new_element_attributes: Attributes of the new element
     :param old_element_text: Text of the element to be replaced
     :param new_element_text: Text of the new element
+    :param old_element_attributes: Attributes of the element to be replaced
+    :param new_element_attributes: Attributes of the new element
     :return: ElementTree of the manipulated xml
     """
     manipulated_xml = deepcopy(xml)
@@ -122,13 +122,13 @@ def remove_element_by_path(xml: ElementTree, element_tag: str) -> ElementTree:
 
 def create_element(
         element_tag: str,
-        element_attributes: dict = None,
-        element_text: str = None) -> Element:
+        element_text: str = None,
+        element_attributes: dict = None) -> Element:
     """
     Simple helper function to create Elements from strings and a dict.
     :param element_tag: Type of tag of the element to be added
-    :param element_attributes: Attributes of the element to be added
     :param element_text: Text of the element to be added
+    :param element_attributes: Attributes of the element to be added
     """
     if element_attributes:
         element = Element(element_tag, element_attributes)
