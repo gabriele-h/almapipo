c09f56daddaa87aac16c2e39d08eeecd80d60b6d
Christoph Schindler <hop@30hopsmax.at>
2020-06-24 18:32:16 +0200

    Add out-of-pkg modules to setup.

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
+from glob import glob
+from os.path import splitext, basename
+
 from setuptools import setup, find_packages
 
 setup(
-        name="alma_rest",
-        packages=find_packages(where="src"),
-        package_dir={"": "src"}
+    name="alma_rest",
+    version="0.0.1",
+    packages=find_packages(where="src"),
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    package_dir={"": "src"},
+    install_requires=[
+        'sqlalchemy~=1.3.0',
+        'requests~=2.24.0',
+    ],
 )
