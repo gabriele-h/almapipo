09b57cc098d656bf5547e895e73c8b0d75800ad1
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-06-29 09:20:08 +0200

    Revert "Refactor `is_this_an_alma_id()` and test."
    This reverts commit c1563da0d3a08d83099d8e94ceffe99bc7e0e108.

--- tests/test_input_read.py
+++ tests/test_input_read.py
@@ -1,40 +1,35 @@
 """Tests for read_input"""
 
-import os
-
 import pytest
 
 import input_read
 
 
 class TestDoesStringEqualAlmaID:
-    set_of_alma_ids = {
-        '9981093873903332', '22447985240003332', '23447985190003332',
-        '61363367390003332', '62363367380003332', '53375846240003332',
-        '81445078000003332'
-    }
-    not_an_alma_id = '813332'
-    string_empty = ""
-    not_a_string = 1234567
+   set_of_alma_ids = {
+      '9981093873903332', '22447985240003332', '23447985190003332',
+      '61363367390003332', '62363367380003332', '53375846240003332',
+      '81445078000003332'
+   }
+   not_an_alma_id = '813332'
+   string_empty = ""
+   not_a_string = 1234567
 
-    @pytest.fixture
-    def set_env_id_suffix(self):
-        old_suffix = os.environ.get('ALMA_REST_ID_INSTITUTIONAL_SUFFIX', '')
-        os.environ['ALMA_REST_ID_INSTITUTIONAL_SUFFIX'] = '3332'
-        yield
-        os.environ['ALMA_REST_ID_INSTITUTIONAL_SUFFIX'] = old_suffix
+   def test_alma_ids_only(self):
+      bool = all(input_read.is_this_an_alma_id(id) for id in self.set_of_alma_ids)
+      assert bool == True
+   
+   def test_containing_other(self):
+      bool = input_read.is_this_an_alma_id(self.not_an_alma_id)
+      assert bool == False
+   
+   def test_empty(self):
+      bool = input_read.is_this_an_alma_id(self.string_empty)
+      assert bool == False
 
-    def test_alma_ids_only(self, set_env_id_suffix):
-        assert all(input_read.is_this_an_alma_id(id_) for id_ in self.set_of_alma_ids)
+   def test_number_not_string(self):
+      bool = input_read.is_this_an_alma_id(self.not_a_string)
+      assert bool == False
 
-    def test_containing_other(self):
-        assert not input_read.is_this_an_alma_id(self.not_an_alma_id)
-
-    def test_empty(self):
-        assert not input_read.is_this_an_alma_id(self.string_empty)
-
-    def test_number_not_string(self):
-        assert not input_read.is_this_an_alma_id(self.not_a_string)
-
-    def test_set_not_string_either(self):
-        assert not input_read.is_this_an_alma_id(self.set_of_alma_ids)
+   def test_set_not_string_either(self):
+      bool = input_read.is_this_an_alma_id(self.set_of_alma_ids)
