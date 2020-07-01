0b645bc8e980ef8a1e1dbe4bb9ab266f5bcec75c
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-07-01 10:55:42 +0200

    Add function to update records by csv list

--- src/alma_rest/alma_rest.py
+++ src/alma_rest/alma_rest.py
@@ -9,6 +9,7 @@ This will import the other modules and do the following:
 
 from logging import getLogger
 from datetime import datetime
+from typing import Callable
 
 from . import db_read_write
 from . import input_read
@@ -24,6 +25,51 @@ job_timestamp = datetime.now()
 # Logfile
 logger = getLogger(__name__)
 logger.info(f"Starting {__name__} with Job-ID {job_timestamp}")
+
+
+def update_records_from_db_via_api_for_csv_list(
+        csv_path: str,
+        api: str,
+        record_type: str,
+        manipulation: Callable[[str, str], str]) -> None:
+    """
+    Fro a list of Alma-IDs given in a CSV file, this function does the following:
+    * Call GET for the Alma-IDs and store it in fetched_records
+    * Manipulate the retrieved record with the manipulation_function
+    * Call PUT with the manipulated data if input for manipulation_function and its output differ
+    * Save the response to the PUT call in put_post_responses
+    * Set status of all API calls in job_status_per_id
+    :param csv_path: Path of the CSV file containing the Alma IDs.
+    :param api: API to call, first path-argument after "almaws/v1" (e. g. "bibs")
+    :param record_type: Type of the record to call the API for (e. g. "holdings")
+    :param manipulation: Function: First argument alma_ids, second data retrieved via GET, return manipulated data.
+    :return: None
+    """
+    db_session = db_read_write.create_db_session()
+    import_csv_to_db_tables(csv_path, 'PUT')
+    list_of_ids = db_read_write.get_list_of_ids_by_status_and_action('new', 'PUT', job_timestamp, db_session)
+    for alma_id, in list_of_ids:
+        record_data = get_record_for_alma_ids(alma_id, api, record_type)
+        if not record_data:
+            db_read_write.update_job_status_for_alma_id('error', alma_id, job_timestamp, db_session, 'PUT')
+        else:
+            new_record_data = manipulation(alma_id, record_data)
+            if not new_record_data:
+                db_read_write.update_job_status_for_alma_id('error', alma_id, job_timestamp, db_session, 'PUT')
+            elif record_data == new_record_data:
+                db_read_write.update_job_status_for_alma_id('error', alma_id, job_timestamp, db_session, 'PUT')
+                logger.error(f"{manipulation} did not carry out a change of {alma_id}.")
+            else:
+                db_read_write.add_put_post_response_to_session(alma_id, record_data, job_timestamp, db_session)
+                db_read_write.update_job_status_for_alma_id('done', alma_id, job_timestamp, db_session, 'PUT')
+    db_session.commit()
+    ids_done = db_read_write.get_list_of_ids_by_status_and_action('done', 'PUT', job_timestamp, db_session)
+    ids_error = db_read_write.get_list_of_ids_by_status_and_action('error', 'PUT', job_timestamp, db_session)
+    ids_new = db_read_write.get_list_of_ids_by_status_and_action('new', 'PUT', job_timestamp, db_session)
+    logger.info(f"Completed POST successfully for {ids_done.count()} record(s).")
+    logger.info(f"Errors were encountered for POST of {ids_error.count()} record(s).")
+    logger.info(f"POST was not handled at all for {ids_new.count()} record(s).")
+    db_session.close()
 
 
 def restore_records_from_db_via_api_for_csv_list(csv_path: str, api: str, record_type: str) -> None:
