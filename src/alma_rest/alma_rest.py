e59ca7007090bccbf9523c6b0c9a49570c22d6aa
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-07-01 14:48:10 +0200

    Log function name not function

--- src/alma_rest/alma_rest.py
+++ src/alma_rest/alma_rest.py
@@ -58,7 +58,7 @@ def update_records_via_api_for_csv_list(
                 db_read_write.update_job_status_for_alma_id('error', alma_id, job_timestamp, db_session, 'PUT')
             elif record_data == new_record_data:
                 db_read_write.update_job_status_for_alma_id('error', alma_id, job_timestamp, db_session, 'PUT')
-                logger.error(f"{manipulation} did not carry out a change of {alma_id}.")
+                logger.error(f"{manipulation.__name__} did not carry out a change of {alma_id}.")
             else:
                 db_read_write.add_put_post_response_to_session(alma_id, record_data, job_timestamp, db_session)
                 db_read_write.update_job_status_for_alma_id('done', alma_id, job_timestamp, db_session, 'PUT')
