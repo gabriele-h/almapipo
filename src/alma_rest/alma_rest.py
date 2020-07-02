9e108344b3eb993aa026f9a6a80671e93591723f
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-07-02 10:27:38 +0200

    Improve logging update for csv list

--- src/alma_rest/alma_rest.py
+++ src/alma_rest/alma_rest.py
@@ -51,16 +51,19 @@ def update_records_via_api_for_csv_list(
     for alma_id, in list_of_ids:
         record_data = get_record_for_alma_ids(alma_id, api, record_type)
         if not record_data:
+            logger.error(f'Could not fetch record {alma_id}.')
             db_read_write.update_job_status_for_alma_id('error', alma_id, job_timestamp, db_session, 'PUT')
         else:
             new_record_data = manipulation(alma_id, record_data)
             if not new_record_data:
+                logger.error(f'Could not manipulate data of record {alma_id}.')
                 db_read_write.update_job_status_for_alma_id('error', alma_id, job_timestamp, db_session, 'PUT')
             elif record_data == new_record_data:
                 db_read_write.update_job_status_for_alma_id('error', alma_id, job_timestamp, db_session, 'PUT')
                 logger.error(f"{manipulation.__name__} did not carry out a change of {alma_id}.")
             else:
                 db_read_write.add_put_post_response_to_session(alma_id, record_data, job_timestamp, db_session)
+                logger.info(f'Record manipulation for {alma_id} successful. Adding to put_post_response.')
                 db_read_write.update_job_status_for_alma_id('done', alma_id, job_timestamp, db_session, 'PUT')
     db_session.commit()
     ids_done = db_read_write.get_list_of_ids_by_status_and_action('done', 'PUT', job_timestamp, db_session)
