879ae5c179cf95a90fa79eaa032282d8caaa0b12
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-07-02 15:56:31 +0200

    Correct return value of manipulation

--- src/alma_rest/alma_rest.py
+++ src/alma_rest/alma_rest.py
@@ -31,7 +31,7 @@ def update_records_via_api_for_csv_list(
         csv_path: str,
         api: str,
         record_type: str,
-        manipulation: Callable[[str, str], str]) -> None:
+        manipulation: Callable[[str, str], bytes]) -> None:
     """
     Fro a list of Alma-IDs given in a CSV file, this function does the following:
     * Call GET for the Alma-IDs and store it in fetched_records
@@ -64,7 +64,7 @@ def update_records_via_api_for_csv_list(
                 db_read_write.update_job_status_for_alma_id('error', alma_id, job_timestamp, db_session, 'PUT')
             else:
                 logger.info(f'Record manipulation for {alma_id} successful. Adding to put_post_response.')
-                update_record_for_alma_ids(alma_id, api, record_type, new_record_data.encode('utf-8'))
+                update_record_for_alma_ids(alma_id, api, record_type, new_record_data)
                 db_read_write.add_put_post_response_to_session(alma_id, new_record_data, job_timestamp, db_session)
                 db_read_write.update_job_status_for_alma_id('done', alma_id, job_timestamp, db_session, 'PUT')
     db_session.commit()
