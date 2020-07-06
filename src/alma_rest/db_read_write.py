158f65e88b501cca6c6b42c44c2f96f14371dea5
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-07-06 13:49:41 +0200

    Compare data sent and received via API

--- src/alma_rest/db_read_write.py
+++ src/alma_rest/db_read_write.py
@@ -16,7 +16,7 @@ except ImportError:
     from typing import MutableMapping
     OrderedDict = MutableMapping
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func, String
 from sqlalchemy.orm import Session, sessionmaker
 
 from . import db_setup
@@ -37,6 +37,63 @@ def main():
 
     db_engine = create_db_engine(True)
     db_engine.connect()
+
+
+def check_data_sent_equals_put_post_response(
+        alma_ids: str,
+        job_timestamp: datetime,
+        session: Session) -> bool:
+    """
+    For given alma_ids and job_timestamp check if the data sent via PUT/POST
+    and the data received in the API's response are the same. If no data is
+    available in one of the two tables (sent_records and put_post_responses)
+    this check will also return False.
+    :param alma_ids: Comma separated string of Alma IDs to identify the record
+    :param job_timestamp: Job that created the entry in source_csv
+    :param session: Session to be used for the check.
+    :return: True if matches, False if non-existent or does not match.
+    """
+    sent_id = func.concat(db_setup.SentRecords.alma_id, db_setup.SentRecords.job_timestamp)
+    received_id = func.concat(db_setup.PutPostResponses.alma_id, db_setup.PutPostResponses.job_timestamp)
+
+    record_sent = session.query(
+        db_setup.SentRecords
+    ).filter_by(
+        job_timestamp=job_timestamp
+    ).filter_by(
+        alma_id=alma_ids
+    )
+
+    record_received = session.query(
+        db_setup.PutPostResponses
+    ).filter_by(
+        job_timestamp=job_timestamp
+    ).filter_by(
+        alma_id=alma_ids
+    )
+
+    bool_query = session.query(
+        db_setup.PutPostResponses
+    ).join(
+        db_setup.SentRecords,
+        sent_id == received_id
+    ).filter(
+        db_setup.SentRecords.alma_record.cast(String) == db_setup.PutPostResponses.alma_record.cast(String)
+    ).filter_by(
+        job_timestamp=job_timestamp
+    ).filter_by(
+        alma_id=alma_ids
+    )
+
+    if record_sent.count() == 0 or record_received.count() == 0:
+        logger.error(f'No data in either sent_records or put_post_responses for {alma_ids} and {job_timestamp}.')
+        return False
+
+    if bool_query.count() > 0:
+        return True
+    else:
+        logger.warning(f'Data in sent_records and put_post_responses for {alma_ids} and {job_timestamp} did not match.')
+        return False
 
 
 def get_value_from_csv_json(
