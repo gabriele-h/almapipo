8b0a138739034b718c69dfdb1747b86248050e3b
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-07-28 16:08:00 +0200

    Complete electronic API

--- src/alma_rest/rest_electronic.py
+++ src/alma_rest/rest_electronic.py
@@ -25,3 +25,40 @@ class EcollectionsApi(rest_call_api.GenericApi):
         logger.info(f'Instantiating {type(self).__name__}.')
 
         super().__init__(base_path)
+
+
+class EservicesApi(rest_call_api.GenericApi):
+    """
+    Make API calls for e-services.
+    """
+    def __init__(self, collection_id: str):
+        """
+        Initialize API for e-collections.
+        """
+        self.collection_id = collection_id
+
+        base_path = f'/electronic/e-collections/{self.collection_id}/'
+
+        logger.info(f'Instantiating {type(self).__name__} with collection_id {self.collection_id}.')
+
+        super().__init__(base_path)
+
+
+class PortfoliosApi(rest_call_api.GenericApi):
+    """
+    Make API calls for e-services.
+    """
+
+    def __init__(self, collection_id: str, service_id: str):
+        """
+        Initialize API for e-collections.
+        """
+        self.collection_id = collection_id
+        self.service_id = service_id
+
+        base_path = f'/electronic/e-collections/{self.collection_id}/e-services/{self.service_id}'
+
+        log_string = f'Instantiating {type(self).__name__} with '
+        log_string += f'collection_id {self.collection_id} and service_id {self.service_id}.'
+
+        super().__init__(base_path)
