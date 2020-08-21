9dbac4b97059efddfae4d68eae933b927b4d6e62
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-08-21 09:55:38 +0200

    Overhaul MARC-extraction from XML
    * Yield dictionaries for the generator
    * Move the TSV-stuff to a script (TBD)
    * Return a format that can be mapped to JSON

--- src/alma_rest/xml_extract.py
+++ src/alma_rest/xml_extract.py
@@ -40,197 +40,72 @@ def extract_marc_for_job_timestamp(job_timestamp: datetime) -> Iterable[dict]:
     :param job_timestamp: Job to extract the data for
     :return: Generator of dictionaries
     """
-    logger.info(f'Getting bibs retrieved on {job_timestamp} from the database and extracting MARC21 categories.')
-    record_generator = db_read_write.get_records_by_timestamp(job_timestamp)
-    column_headers = create_list_of_marc_fields(record_generator)
+    logger.info(f'Getting bibs retrieved on {job_timestamp} from the database.')
+    db_record_generator = db_read_write.get_records_by_timestamp(job_timestamp)
 
-    for record in db_read_write.get_records_by_timestamp(job_timestamp):
-        yield extract_field_contents_from_marc(record, column_headers)
+    for db_record in db_record_generator:
+        marc_dict = extract_contents_from_marc(db_record.find('record'))
+        yield marc_dict
 
 
-def extract_field_contents_from_marc(record: Element, column_headers: Iterable[str]) -> dict:
+def extract_contents_from_marc(record: Element) -> dict:
     """
-    For a given record extract the information as per column_headers list.
+    For a given record-element extract the information as per column_headers list.
     The latter may be generated automatically for a list of records
     with the create_list_of_marc_fields function.
     :param record: MARC21 record as xml.ElementTree.Element
-    :param column_headers: List of strings with keys to find elements by
-    :return: Dictionary of values retrieved by keys given in column_headers
+    :return: Dictionary representation of the record.
     """
-    marc21_dict = {'LDR': record.find('record/leader').text}
+    marc21_dict = {'leader': record.find('leader').text}
 
-    for marc_key in column_headers:
+    for controlfield in record.findall('controlfield'):
+        tag = controlfield.attrib['tag']
+        text = controlfield.text
+        append_multiple_to_dict(marc21_dict, tag, text)
 
-        content = ''
-        superfields = record.findall(f'record/*[@tag="{marc_key[0:3]}"]')
-
-        if '_' in marc_key:
-            num_suffix = int(marc_key.split('_')[1])
-            num_repeat = num_suffix - 1
-        else:
-            num_repeat = 0
-
-        try:
-            ind1 = marc_key[3]
-            ind2 = marc_key[4]
-            sf_code = marc_key[5]
-        except IndexError:
-            try:
-                content = superfields[num_repeat].text
-            except IndexError:
-                content = ''
-        else:
-
-            xpath = f'[@ind1="{ind1}"][@ind2="{ind2}"]/subfield[@code="{sf_code}"]'
-
-            try:
-                field = superfields[num_repeat].findall(xpath)[0]
-            except IndexError:
-                pass
-            else:
-                content = field.text
-
-        marc21_dict[marc_key] = content or ''
+    for datafield in record.findall('datafield'):
+        tag, ind1, ind2 = datafield.attrib['tag'], datafield.attrib['ind1'], datafield.attrib['ind2']
+        datafield_dict = {'ind1': ind1, 'ind2': ind2}
+        subfields = extract_subfields_from_datafield(datafield)
+        datafield_dict.update(subfields)
+        append_multiple_to_dict(marc21_dict, tag, datafield_dict)
 
     return marc21_dict
 
 
-def create_list_of_marc_fields(record_generator: Iterable[Element]) -> Iterable[str]:
+def extract_subfields_from_datafield(datafield: Element) -> dict:
     """
-    For a given generator of MARC21 records, create a list of keys consisting
-    of the tag, and where applicable ind1, ind2 and subfield code. The leader will have the
-    key "LDR".
-    :param record_generator: Generator containing MARC21 records as etree.Element
-    :return: List of strings containing keys of MARC21 categories
+    For a given datafield element extract a dictionary of
+    its subfields, where the key is the attribute "code" and
+    the value the subfield's text.
+    :param datafield:
+    :return: Dictionary with subfield-code as key and subfield-text as value.
     """
-    controlfield_headers = []
-    datafield_headers = []
-    counters = {}
+    dict_of_subfields = {}
 
-    for record in record_generator:
-        controlfield_headers, counters = extract_controlfield_keys_from_marc(record, controlfield_headers, counters)
-        datafield_headers, counters = extract_datafield_keys_from_marc(record, datafield_headers, counters)
+    for subfield in datafield.findall('subfield'):
+        code = subfield.attrib['code']
+        text = subfield.text
+        append_multiple_to_dict(dict_of_subfields, code, text)
 
-    controlfield_headers_sorted = sorted(controlfield_headers, key=lambda x: x[0:7])
-    datafield_headers_sorted = sorted(datafield_headers, key=lambda x: x[0:7])
-
-    column_headers = controlfield_headers_sorted + datafield_headers_sorted
-
-    return column_headers
+    return dict_of_subfields
 
 
-def extract_controlfield_keys_from_marc(
-        record: Element,
-        column_headers: Iterable[str],
-        counters: dict) -> Tuple[Iterable[str], dict]:
+def append_multiple_to_dict(dictionary: dict, key: str, value) -> dict:
     """
-    For a given record appends descriptors of controlfields to the list
-    column_headers provided in the second parameter.
-    :param record: MARC21 record as etree.Element
-    :param column_headers: List of strings containing keys of MARC21 categories
-    :param counters: Keep count of all fields across all records
-    :return: Manipulated list of column_headers
+    For a given dictionary see if the key already exists and if
+    it does, append the value to the list of values.
+    :param dictionary: Dictionary to add the value to
+    :param key: Potentially repeated key to check
+    :param value: Value to add new key or append if key exists
+    :return: Manipulated dictionary
     """
-    controlfields = record.findall('record/controlfield')
-    local_counters = {}
+    try:
+        dictionary[key]
+    except KeyError:
+        dictionary.update({key: [value]})
+    else:
+        new_values = dictionary[key] + [value]
+        dictionary[key] = new_values
 
-    for controlfield in controlfields:
-
-        controlfield_tag = controlfield.attrib['tag']
-
-        try:
-            local_counters[controlfield_tag]
-        except KeyError:
-            local_counters[controlfield_tag] = 1
-        else:
-            local_counters[controlfield_tag] += 1
-
-        try:
-            counters[controlfield_tag]
-        except KeyError:
-            counters[controlfield_tag] = 1
-        else:
-            if local_counters[controlfield_tag] > counters[controlfield_tag]:
-                counters[controlfield_tag] = local_counters[controlfield_tag]
-
-        if controlfield_tag not in column_headers:
-            column_headers.append(controlfield_tag)
-
-    return column_headers, counters
-
-
-def extract_datafield_keys_from_marc(
-        record: Element,
-        column_headers: Iterable[str],
-        counters: dict) -> Tuple[Iterable[str], dict]:
-    """
-    For a given record appends descriptors of datafields to the list
-    column_headers provided in the second parameter.
-    :param record: MARC21 record as etree.Element
-    :param column_headers: List of strings containing keys of MARC21 categories
-    :param counters: Keep count of all fields across all records
-    :return: Manipulated list of column_headers
-    """
-    datafields = record.findall('record/datafield')
-    local_counters = {}
-
-    for datafield in datafields:
-
-        datafield_tag = datafield.attrib['tag']
-
-        try:
-            datafield_ind1 = datafield.attrib['ind1']
-        except KeyError:
-            datafield_ind1 = None
-
-        try:
-            datafield_ind2 = datafield.attrib['ind2']
-        except KeyError:
-            datafield_ind2 = None
-
-        for subfield in datafield.findall('subfield'):
-
-            try:
-                subfield_code = subfield.attrib['code']
-            except KeyError:
-                logger.warning('No subfield code attribute?')
-            else:
-
-                datafield_description = datafield_tag
-
-                if datafield_ind1:
-                    datafield_description += datafield_ind1
-
-                if datafield_ind2:
-                    datafield_description += datafield_ind2
-
-                if subfield_code:
-                    datafield_description += subfield_code
-
-                try:
-                    local_counters[datafield_description]
-                except KeyError:
-                    local_counters[datafield_description] = 1
-                else:
-                    local_counters[datafield_description] += 1
-
-                try:
-                    counters[datafield_description]
-                except KeyError:
-                    counters[datafield_description] = 1
-                else:
-                    if local_counters[datafield_description] > counters[datafield_description]:
-                        counters[datafield_description] = local_counters[datafield_description]
-
-                if datafield_description not in column_headers:
-                    column_headers.append(datafield_description)
-
-                if counters[datafield_description] > 1:
-                    max_datafield_description = datafield_description+'_'+str(counters[datafield_description])
-                    if max_datafield_description not in counters:
-                        for i in range(2, counters[datafield_description]):
-                            datafield_description_with_counter = datafield_description+'_'+str(i)
-                            if datafield_description_with_counter not in column_headers:
-                                column_headers.append(datafield_description_with_counter)
-
-    return column_headers, counters
+    return dictionary
