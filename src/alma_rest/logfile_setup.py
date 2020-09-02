e0730c412b21e987163a086777c703343692f2eb
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-09-02 12:45:01 +0200

    Fix path creation for logfile dir

--- src/alma_rest/logfile_setup.py
+++ src/alma_rest/logfile_setup.py
@@ -2,12 +2,13 @@
 
 import logging
 from os import environ
+from pathlib import Path
 
 log_format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 log_formatter = logging.Formatter(log_format_string)
 
-logfile_dir_path = environ['ALMA_REST_LOGFILE_DIR']
-logfile_path = logfile_dir_path + 'alma_rest.log'
+logfile_dir_path = Path(environ['ALMA_REST_LOGFILE_DIR'])
+logfile_path = logfile_dir_path / 'alma_rest.log'
 logging.basicConfig(
     format=log_format_string,
     filename=logfile_path,
