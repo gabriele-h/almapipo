cdab8c500e7ef9b26d6ae5086d8d13f1cbd38f7d
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2021-01-18 13:04:42 +0100

    Correct requests headers

--- src/alma_rest/rest_setup.py
+++ src/alma_rest/rest_setup.py
@@ -274,9 +274,9 @@ def create_alma_api_session(session_format) -> Session:
 
     session.headers.update({
         "accept": "application/" + session_format,
-        "Content-Type": "application/" + session_format,
+        "Content-Type": f"application/{session_format}; charset=utf-8",
         "authorization": f"apikey {api_key}",
-        "User-Agent": f"alma_rest/{metadata.version}"
+        "User-Agent": f"alma_rest/{metadata.version('alma_rest')}"
     })
 
     return session
