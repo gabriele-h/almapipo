1afd47e7299b0c061c638ce03a5cc4a87b0409e4
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2022-05-12 13:49:31 +0200

    Add threadName to logging

--- src/almapipo/setup_logfile.py
+++ src/almapipo/setup_logfile.py
@@ -4,7 +4,8 @@ import logging
 from os import environ
 from pathlib import Path
 
-log_format_string = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+log_format_string = "%(asctime)s - %(name)s - %(threadName)s - " \
+                    "%(levelname)s - %(message)s"
 log_formatter = logging.Formatter(log_format_string)
 
 logfile_dir_path = Path(environ["ALMA_REST_LOGFILE_DIR"])
